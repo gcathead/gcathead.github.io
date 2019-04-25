0316d0fa090d18661ff40d887958c0097b7c64da
cathead <32792025+gcathead@users.noreply.github.com>
2019-04-25 17:22:32 +0700

    Add files via upload

---
+++ reward.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Главная</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/reward.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+	<!--Шапка сайта-->
+	<header>
+		<ul class="top_menu">
+			<li><i class="fa fa-bars" aria-hidden="true"></i>
+				<ul class="sub_menu">
+					<a href="index.php"><li>Главная</li></a>
+					<a href="news.php"><li>Новости</li></a>
+					<a href="college.php"><li>О колледже</li></a>
+					<a href="museum.php"><li>О музее</li></a>
+					<a href="reward.php"><li>Награды</li></a>
+					<a href="memory.php"><li>Вечная память</li></a>
+					<a href="panorama.php"><li>Фотогалерея</li></a>
+					<a href="respect.php"><li>Почётные работники</li></a>
+				</ul>
+			</li>
+		</ul>
+		<h4>Аэрокосмический колледж СибГУ имени академика М. Ф. Решетнева</h4>
+		<div class="right_side">
+			<i class='fa fa-search' aria-hidden='true'></i>
+			<input type="text">
+		</div>
+	</header>
+	<main>
+		<h1>Награды</h1>
+		
+	</main>
+	<footer>
+		&#169; 2019 Все права защещены АК СИБГУ Политика конфиденциальности
+	</footer>
+</body>
+</html>
